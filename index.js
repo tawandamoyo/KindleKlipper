04cfb98e6567dec4fcf8b19573cd519de4eed4a6
Abel McElroy <abel.h.mcelroy@gmail.com>
2022-10-06 14:13:56 -0400

    code review

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const { connected } = require('process');
-
 const fs = require('fs');
 const path = require('path');
-const os = require('os');
 
 const pathName = path.join(__dirname, '/books/');
 
@@ -11,7 +8,7 @@ const file = process.argv[2];
 
 fs.mkdir(pathName, { recursive: true }, (error) => {
     if(error) {
-        console.log('failed to make dir')
+        throw error;
     }
     else {
         console.log(`successfully created the dir: ${pathName}`);
@@ -21,33 +18,25 @@ fs.mkdir(pathName, { recursive: true }, (error) => {
 
 fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
-        return console.log(err)
+        throw err;
     }
 
     // the data is split into a new array called clippings
     const clippings = data.split('==========');
 
     // declare an empty array that will hold the books & highlights
-    let books = [];
-
-    // searchBooks searches in 'books' array & returns index of book if it exists
-    function searchBooks(books, title) {
-        for(let i = 0; i < books.length; i++) {
-            if (books[i].title === title) {
-                bookIndex = i;
-                return;
-            }
-        }
-        bookIndex = -1;
-        return bookIndex;
-    }
+    const books = {};
 
     for(let i = 0; i < clippings.length; i++) {
 
         // split each clipping in clippings into component parts whenever there's a new line
-        let clipping = clippings[i].split('\r\n');
+        let clipping = clippings[i].split('\n');
         clipping = clipping.filter(e => e.length);
 
+        // in case the entry is blank (seems like the last entry is always blank based on the txt file format)
+        if (clipping.length === 0) {
+          continue; // a way to skip a single iteration of a loop;
+        }
         // extract and assign title (and author) to title
         // TODO: separate title and author
         let title = clipping[0];
@@ -59,48 +48,62 @@ fs.readFile(file, 'utf8', (err, data) => {
            - these are in clipping[1], need to extract the relevant parts
         */
 
-        // when called this function creates a new book Object using the details above
-       function createBook (title, highlight) {
-        let book = {
-            title: title,
-            highlights: [highlight]
-        }
-        return book;
-       }
-
-        if (!books.length) {
-            //if books is empty, call createBooks and push the created Object to books
-            let book = createBook(title, highlight);
-            books.push(book);
+        if (books[title]) {
+          books[title].push(highlight);
         }
         else {
-            //if array is not empty then we must search if a book exists and find its index
-            searchBooks(books, title);
-
-            if (bookIndex >= 0) {
-                //if the book exists then we only need to push the highlight to the highlights array
-                books[bookIndex].highlights.push(highlight);
-            }
-            else {
-                //if book does not exist then create it
-                let book = createBook(title, highlight);
-                books.push(book);
-            }
-        }   
+          books[title] = [highlight];
+        }
     }
 
     // Loop through each book and write its contents to txt file
 
-    books.forEach((book) => {
-        txtFilePath = pathName + book.title + '.txt';
-        for (let i = 0; i < book.highlights.length; i++) {
-            fs.appendFile(txtFilePath, book.highlights[i] + "\n\n", (error) => {
-                if (error) {
-                    console.log(error);
-                }
-            });
-        }
-    });
-    
+    Object.keys(books).forEach((title) => {
+      const highlights = books[title];
+      txtFilePath = pathName + title + '.txt';
 
+      for (let i = 0; i < highlights.length; i++) {
+        // conditional write function since we shouldn't append to the same files again and again when we run the script multiple times
+        function write(text) {
+          if (i === 0) {
+            fs.writeFile(txtFilePath, text + "\n\n", (err) => {
+              if (err) throw err;
+            })
+          } else {
+            fs.appendFile(txtFilePath, text + "\n\n", (err) => {
+              if (err) throw err;
+            })
+          }
+        }
+        
+        write(highlights[i]);
+      }
+    });
 });
+
+// Using fs sync methods with utf8 encoding because I expect the file sizes won't be TOO big 
+// anything larger than (or even approaching) the memory allocation of the Node.js runtime will cause issues
+// that can be resolved by using readstreams and processing files chunk by chunk, but the logic is far trickier to write
+function verySimplified(inputFilePath = process.argv[2]) {
+  const booksDir = path.join(__dirname, "books");
+  // making our books dir if it doesn't exist
+  const booksDirExists = fs.existsSync(booksDir);
+  if (!booksDirExists) fs.mkdirSync(booksDir);
+
+  const imputFileText = fs.readFileSync(inputFilePath, "utf8");
+  const previouslySeenTitles = new Set();
+
+  // looping over entries from the input file
+  imputFileText.split(/={10}/).forEach(clipping => {
+    // parsing the title and highlight from each entry
+    const lines = clipping.split("\n").filter(line => line);
+    if (lines.length === 0) return;
+    const [title, _, highlight] = lines;
+
+    // conditionally writing/appending the highlight to output file
+    const seenBefore = previouslySeenTitles.has(title);
+    const bookFilePath = path.join(__dirname, "books", `${title}.txt`);
+    fs[seenBefore ? "appendFileSync" : "writeFileSync"](bookFilePath, `${seenBefore && "\n\n"}${highlight}`, "utf8");
+    previouslySeenTitles.add(title);
+  });
+}
